ecb4bf4e596bb628bebeba7a900847a739d78ce0
Cristobal <kris_todosinfin@outlook.com>
2021-05-04 11:54:30 -0500

    primer commit

---
+++ src/mispruebas/Test.java
@@ -0,0 +1,14 @@
+
+package mispruebas;
+
+
+public class Test {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello world ;)");
+        
+        
+        
+        
+    }
+}
